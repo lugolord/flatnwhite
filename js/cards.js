f28f7c426ecfc137813dfbba93eaedfbec0ee8c8
Luis Gonzalez <lugo50635@gmail.com>
2021-04-18 21:09:47 -0300

    x

--- js/cards.js
+++ js/cards.js
@@ -6,7 +6,7 @@ $.getJSON(products, function (respuesta, estado) {
         let misProductos = respuesta;
 
         for (const producto of misProductos.productos) {
-            $('#productos').append(`<div class="col-12 col-xl-3 pl-0 pr-0 mb-5">
+            $('#productos').append(`<div class="col-12 col-sm-6 col-xl-3 pl-0 pr-0 mb-5">
                                       <img src="${producto.src}" class="col-12">
                                       <p class="col-12 mb-0">${producto.nombre}</p>
                                       <p class="col-12" style="color: green;">$${producto.precio}</p>
