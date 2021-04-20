81c668fd608e2f01f4c4364925a93e2954d31352
Luis Gonzalez <lugo50635@gmail.com>
2021-04-19 23:06:18 -0300

    index responsive

--- js/functions.js
+++ js/functions.js
@@ -97,7 +97,7 @@ function construirSeccion(x) { //CONSTRUYE LAS CARDS POR CATEGORIA
         $('#productos').fadeOut(800, function() {
             $('#productos').empty();
             for (const whatever of item) {
-                $('#productos').append(`<div class="col-12 col-xl-3 pl-0 pr-0 p-xl-0 mb-5">
+                $('#productos').append(`<div class="col-12 col-sm-6 col-md-4 col-xl-3 pl-0 pr-0 p-xl-0 mb-5">
                                           <img src="${whatever.src}" class="col-12">
                                           <p class="col-12 mb-0">${whatever.nombre}</p>
                                           <p class="col-12" style="color: green;">$${whatever.precio}</p>
@@ -144,7 +144,7 @@ function filtrar() { //FILTRA LOS RESULTADOS BUSCADOS EN LA BARRA DE BUSQUEDA
             let nombre = producto.nombre.toLowerCase();
 
             if (nombre.indexOf(texto) !== -1) {
-                $('#productos').append(`<div class="col-12 col-xl-3 pl-0 pr-0 p-xl-0 mb-5">
+                $('#productos').append(`<div class="col-12 col-sm-6 col-md-4 col-xl-3 pl-0 pr-0 p-xl-0 mb-5">
                                           <img src="${producto.src}" class="col-12">
                                           <p class="col-12 mb-0">${producto.nombre}</p>
                                           <p class="col-12" style="color: green;">$${producto.precio}</p>
@@ -154,9 +154,9 @@ function filtrar() { //FILTRA LOS RESULTADOS BUSCADOS EN LA BARRA DE BUSQUEDA
         }
         if ($('#productos').html() === '') {
             $('#productos').html(`<div class="col-12 text-center" style="height: 50vh;">
-                                      <p style="font-size: 50px; position: absolute; top: 25%; left: 25%;">No se encontraron productos</p>
-                                      <i class="far fa-sad-tear fa-4x"></i>
-                                    </div>`);
+                                    <p id="sinResultados">No se encontraron productos</p>
+                                    <i class="far fa-sad-tear fa-4x"></i>
+                                  </div>`);
         }
 
         carrito();
--- js/sections.js
+++ js/sections.js
@@ -3,7 +3,7 @@ $('#seccionTodo').click(function (e) {
     $('#productos').fadeOut(800, function() {
         $('#productos').empty();
         for (const producto of productos) {
-            $('#productos').append(`<div class="col-12 col-xl-3 pl-0 pr-0 p-xl-0 mb-5">
+            $('#productos').append(`<div class="col-12 col-sm-6 col-md-4 col-xl-3 pl-0 pr-0 p-xl-0 mb-5">
                                       <img src="${producto.src}" class="col-12">
                                       <p class="col-12 mb-0">${producto.nombre}</p>
                                       <p class="col-12" style="color: green;">$${producto.precio}</p>
